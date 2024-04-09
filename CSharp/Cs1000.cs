25f2b81e201fc706aad6a953acc1a843ae72b8db
Alisson Lima <alissonlima50138@gmail.com>
2024-04-09 14:30:08 -0300

    cs1000

---
+++ CSharp/Cs1000.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp
+{
+    public class Cs1000
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
