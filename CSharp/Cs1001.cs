1184c7ad1d75bfc4fcc8af6f87e64c9cf91d8e73
Alisson Lima <alissonlima50138@gmail.com>
2024-04-09 14:32:46 -0300

    Create Cs1001.cs

---
+++ CSharp/Cs1001.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace CSharp
+{
+    public class Cs1001
+    {
+        public static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+
+            int X = A + B;
+
+            Console.WriteLine("X = {0}", X);
+        }
+    }
+}
